404482ba8949104390ed5368e0c71b56a8df8e38
nguyenvhung9420 <vanhung.nguyen2@my.jcu.edu.au>
2020-04-29 22:41:59 +0800

    29 April

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -2,6 +2,9 @@ package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,31 +12,102 @@ public class MainActivity extends Activity {
 
 
     // TODO: Declare member variables here:
+    android.widget.Button buttonTrue;
+    android.widget.Button buttonFalse;
+    android.widget.TextView mQuestionTextView;
+    android.widget.ProgressBar mProgressBar;
+    android.widget.TextView mScoreTextView;
+    int mIndex = 0;
+    int mScore = 0;
 
 
     // TODO: Uncomment to create question bank
-//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-//            new TrueFalse(R.string.question_1, true),
-//            new TrueFalse(R.string.question_2, true),
-//            new TrueFalse(R.string.question_3, true),
-//            new TrueFalse(R.string.question_4, true),
-//            new TrueFalse(R.string.question_5, true),
-//            new TrueFalse(R.string.question_6, false),
-//            new TrueFalse(R.string.question_7, true),
-//            new TrueFalse(R.string.question_8, false),
-//            new TrueFalse(R.string.question_9, true),
-//            new TrueFalse(R.string.question_10, true),
-//            new TrueFalse(R.string.question_11, false),
-//            new TrueFalse(R.string.question_12, false),
-//            new TrueFalse(R.string.question_13,true)
-//    };
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_1, true),
+            new TrueFalse(R.string.question_2, true),
+            new TrueFalse(R.string.question_3, true),
+            new TrueFalse(R.string.question_4, true),
+            new TrueFalse(R.string.question_5, true),
+            new TrueFalse(R.string.question_6, false),
+            new TrueFalse(R.string.question_7, true),
+            new TrueFalse(R.string.question_8, false),
+            new TrueFalse(R.string.question_9, true),
+            new TrueFalse(R.string.question_10, true),
+            new TrueFalse(R.string.question_11, false),
+            new TrueFalse(R.string.question_12, false),
+            new TrueFalse(R.string.question_13, true)
+    };
+
+    int PROGRESS_INCREMENT = (int) Math.ceil(100 / mQuestionBank.length);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonTrue = findViewById(R.id.true_button);
+        buttonFalse = findViewById(R.id.false_button);
+        mScoreTextView = findViewById(R.id.score);
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        mQuestionTextView.setText(mQuestionBank[mIndex].getQuestionID());
+
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        if (savedInstanceState != null){
+            mScore = savedInstanceState.getInt("ScoreKey");
+            mIndex = savedInstanceState.getInt("IndexKey");
+            mScoreTextView.setText(String.format("Score: %s/%s", mScore, mQuestionBank.length));
+        } else {
+            mScore = 0;
+            mIndex = 0;
+        }
+
+        buttonTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mQuestionBank[mIndex].isAnswer() == true) {
+                    Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+                    addScore();
+                    updateQuestion();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Answer :(", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
+
+        buttonFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mQuestionBank[mIndex].isAnswer() == false) {
+                    Toast.makeText(getApplicationContext(), "Correct", Toast.LENGTH_SHORT).show();
+                    addScore();
+                    updateQuestion();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Answer :(", Toast.LENGTH_SHORT).show();
+                    updateQuestion();
+                }
+            }
+        });
 
 
+    }
+
+    private void addScore() {
+        mScore++;
+        mScoreTextView.setText(String.format("Score: %s/%s", mScore, mQuestionBank.length));
+    }
+
+    private void updateQuestion() {
+        mIndex = (mIndex + 1) % mQuestionBank.length;
+        mProgressBar.incrementProgressBy(PROGRESS_INCREMENT);
+        mQuestionTextView.setText(mQuestionBank[mIndex].getQuestionID());
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("ScoreKey", mScore);
+        outState.putInt("IndexKey", mIndex);
     }
 }
